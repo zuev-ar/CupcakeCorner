e5a183ee0d721482547e077e785d909431053e39
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-05-31 22:21:48 +0500

    project 10, part 1

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,36 +7,36 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
-class User: ObservableObject, Codable {
-    @Published var name = "Arkasha Zuev"
-    
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-}
+//struct Response: Codable {
+//    var results: [Result]
+//}
+//
+//struct Result: Codable {
+//    var trackId: Int
+//    var trackName: String
+//    var collectionName: String
+//}
+//
+//class User: ObservableObject, Codable {
+//    @Published var name = "Arkasha Zuev"
+//
+//    enum CodingKeys: CodingKey {
+//        case name
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(name, forKey: .name)
+//    }
+//
+//    required init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        name = try container.decode(String.self, forKey: .name)
+//    }
+//}
 
 struct ContentView: View {
-    @State private var results = [Result]()
+//    @State private var results = [Result]()
     @State private var username = ""
     @State private var email = ""
     
@@ -69,24 +69,24 @@ struct ContentView: View {
         }
     }
     
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    DispatchQueue.main.async {
-                        self.results = decodedResponse.results
-                    }
-                    return
-                }
-            }
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
-    }
+//    func loadData() {
+//        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+//            print("Invalid URL")
+//            return
+//        }
+//        let request = URLRequest(url: url)
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+//                    DispatchQueue.main.async {
+//                        self.results = decodedResponse.results
+//                    }
+//                    return
+//                }
+//            }
+//            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+//        }.resume()
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
